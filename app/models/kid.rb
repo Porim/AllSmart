f2cab55c1aaa336c1d036bdb5e26728cd08686df
dobrin nedyalkov <dob.nedyalkov@gmail.com>
2020-08-24 17:59:41 +0100

    created seed for fake users, kids, games and allowed_games

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,6 +1,6 @@
 class Kid < ApplicationRecord
   belongs_to :user
-  has_many :allowed_games
+  has_many :allowed_games, dependent: :destroy
 
   validates :age, presence: true, numericality: { only_integer: true }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Kid.destroy_all
+AllowedGame.destroy_all
+User.destroy_all
+puts "creating fake games"
+subjects = %w(Math English Biology Science)
+difficulty_levels = [1, 2, 3, 4, 5]
+age_categories = %w(4-5 5-7)
+subjects.each do |subject|
+  Game.create(subject: subject, difficulty_level: difficulty_levels.sample, age_category: age_categories.sample, base_score: 20)
+end
+puts "Finished"
+puts "creating 10 fake users with 2 fake kids each"
+10.times do 
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  user = User.new(first_name: first_name, last_name: last_name, email: Faker::Internet.email, password: ("a".."z").to_a.sample(8))
+  user.save
+  2.times do
+    kid = Kid.create(first_name: Faker::Name.first_name, last_name: last_name, age: [4, 5, 6, 7].sample, user_id: user.id)
+    Game.all.each do |game|
+      AllowedGame.create(game_id: game.id, kid_id: kid.id)
+    end
+  end
+end
+puts "Finished"
