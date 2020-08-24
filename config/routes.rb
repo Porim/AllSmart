19a36f7e1a0f54d4c3929b0f761f9a72103c45e5
dobrin nedyalkov <dob.nedyalkov@gmail.com>
2020-08-24 17:24:33 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resouces :users, only: []
+  resources :games, only: [:show, :index]
+  resources :kids, only: [:show, :create, :index] do
+  resources :allowed_games, only: [:index, :show, :create, :destroy]
+  end
+  get "/profile", to: "users#profile", as: :profile
 end
