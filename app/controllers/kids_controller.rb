e0dced5d8607a773aed15724283b1b9d70f6fd8e
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-08-25 13:45:29 +0100

    added show create index edit new actions for kids controller and corresponding views

---
+++ app/controllers/kids_controller.rb
@@ -0,0 +1,42 @@
+class KidsController < ApplicationController
+  before_action :set_kid, only: [:edit, :show, :update]
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @kid.update(kid_params)
+      redirect_to kid_path(@kid)
+    else
+      render :edit
+    end
+  end
+  
+  def new
+    @kid = Kid.new
+  end
+  
+  def create
+    @kid = Kid.new(kid_params)
+    @kid.user = current_user
+    if @kid.save
+      redirect_to profile_path(current_user)
+    else 
+      render :new
+    end
+  end
+  
+  private
+  
+  def kid_params
+  params.require(:kid).permit(:age, :first_name, :last_name)
+  end
+  
+  def set_kid
+    @kid = Kid.find(params[:id]) 
+  end
+  
+end
