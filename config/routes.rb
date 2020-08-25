438030b8d4eb4a0f85a655f1279b4ca5f9b188d6
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-08-25 17:13:16 +0100

    basic child profile page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resouces :users, only: []
+
   get "/profile", to: "users#profile", as: :profile
+  get "/kids/:id/profile", to: "kids#profile", as: :kids_profile
   resources :games, only: [:show, :index]
   resources :kids, only: [:show, :create, :index, :edit, :update, :new] do
   resources :allowed_games, only: [:index, :show, :create, :destroy, :new]
