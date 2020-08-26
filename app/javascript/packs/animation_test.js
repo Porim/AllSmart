f0f9ce6b8dd41f1377049b18d96b113a41474fa0
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-08-26 10:49:08 +0100

    fixed js animation a bit

--- app/javascript/packs/animation_test.js
+++ app/javascript/packs/animation_test.js
@@ -2,12 +2,15 @@ const myMove = () => {
   const elem = document.getElementById("rocket");
   let pos = 100;
   const id = setInterval(frame, 10);
-
+  console.log(window.innerWidth);
+  console.log(window.innerHeight);
   function frame() {
-    if (pos == 1800) {
+    if (pos >= window.innerWidth +100) {
       clearInterval(id);
+      elem.style.setProperty('display', 'none', 'important');
     } else {
       pos += 1.5;
+      console.log(pos);
       elem.style.left = pos + 'px';
       // elem.style.left = pos + 'px';
     }
