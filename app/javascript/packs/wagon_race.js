741e98488a5cb9323fc0522a99ed9f8413dc8064
dobrin nedyalkov <dob.nedyalkov@gmail.com>
2020-09-01 12:19:40 +0100

    added the smart wagon race to games/show page

--- app/javascript/packs/wagon_race.js
+++ app/javascript/packs/wagon_race.js
@@ -1,3 +1,21 @@
+const ask = () => {
+  let x = Math.floor(Math.random() * 5) + 1;
+  let y = Math.floor(Math.random() * 4) + 1;
+  let op = ["+", "-"][Math.floor(Math.random() * 2)];
+  let problem = 0;
+  if ((x - y) > 0) {
+    problem = { question: `How much is ${x}${op}${y}`, answer: eval(x + op + y) };
+  } else {
+    problem = { question: `How much is ${x} + ${y}`, answer: eval(`${x} + ${y}`) };
+  }
+  return problem;
+
+};
+console.log(ask());
+
+const questions = [ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask()];
+console.log(questions);
+
 const moveForward = (player) => {
   const wagon = document.querySelector(`#player${player}-race .active`);
   if (wagon.nextElementSibling) {
@@ -9,14 +27,21 @@ const moveForward = (player) => {
   }
 };
 
-const moveWagons = (event) => {
-  if (event.key === "q") {
-    moveForward(1);
-  } else if (event.key === "p") {
-    moveForward(2);
+const displayQuestion = (index) => {
+  if (index < 20) {
+    quiz.innerText = questions[index].question;
+    document.addEventListener("keyup", (event) => {
+      if (event.key == questions[index].answer) {
+        moveForward(1);
+        displayQuestion(index + 1);
+      } else {
+        moveForward(2);
+        displayQuestion(index + 1);
+      }
+    }, { once: true });
   }
 };
 
-document.addEventListener("keyup", moveWagons);
-
-export { moveForward, moveWagons }
+const index = 0;
+const quiz = document.querySelector(".question");
+displayQuestion(index);
