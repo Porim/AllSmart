bd069bac01c98291b9c22e979e32d323db564b14
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-09-01 11:40:33 +0100

    fixed links on cards and subject icons on landing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # post "/games", to: 'games#index'
+    get "/games?subject=All", to: "games#all", as: :all_games
+  get "/games?subject=Maths", to: "games#maths", as: :maths
+  get "/games?subject=Science", to: "games#science", as: :science
+  get "/games?subject=Biology", to: "games#biology", as: :biology
+  get "/games?subject=English", to: "games#english", as: :english
   get "/profile/:user_id", to: "users#profile", as: :profile
   root to: 'pages#home'
   get "/kids/:id/profile", to: "kids#profile", as: :kids_profile
