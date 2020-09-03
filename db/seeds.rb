d601709351f08ffb86e3d26fbe3f9cd4a89d4b07
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-09-03 10:17:49 +0100

    added proper names to seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,7 +70,7 @@ end
 puts 'Creating test user with kids and games'
 
 me = User.create(first_name: 'Nikita', last_name: 'Visencuk', email: 'nikita@visencuk.com', password: '123456')
-test_kids = [Kid.create(first_name: 'Kid A', last_name: 'Clever', age: [4, 5, 6, 7].sample, user: me), Kid.create(first_name: 'Kid B', last_name: 'Also Clever', age: [4, 5, 6, 7].sample, user: me)]
+test_kids = [Kid.create(first_name: 'Paul', last_name: 'Visencuk', age: [4, 5, 6, 7].sample, user: me), Kid.create(first_name: 'Anne', last_name: 'Visencuk', age: [4, 5, 6, 7].sample, user: me)]
 
 test_kids.each do |kid|
   Game.all.each do |game|
