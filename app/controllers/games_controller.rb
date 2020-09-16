b7faed293c91860e8ecdd82bd67016e809ccac69
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-09-16 12:20:33 +0100

    added set_kid set_game methods to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,30 +1,10 @@
 class GamesController < ApplicationController
+  before_action :set_kid, :set_game, only: [:show]
+  
   def index
-
     @subject = params[:subject].nil? ? 'All' : params[:subject]
     @subjects = { 'Maths': 'medolwcf6nigmrl3cvev', 'Science': 'rnrgntaicfrllbnrzlxg', 'English': 'dkbxbzo5dpzzf0o3kbgy', 'Biology': 'enssmizahvyzkryyufw0' }
     @all_games = Game.all
-    # if params[:subject] == 'Biology'
-    #   @game_title = "Human Biology"
-    #   @subject_image = 'enssmizahvyzkryyufw0'
-    #   @biology = 1
-    # elsif params[:subject] == 'All'
-    #   @game_title = "All Games"
-    #   @subject_image = 'uiprme0fpeh7am66dntp'
-    #   @all = 1
-    # elsif params[:subject] == 'Maths'
-    #   @game_title = params[:subject]
-    #   @subject_image = 'medolwcf6nigmrl3cvev'
-    #   @maths = 1
-    # elsif params[:subject] == 'English'
-    #   @game_title = 'English'
-    #   @subject_image = 'dkbxbzo5dpzzf0o3kbgy'
-    #   @english = 1
-    # else
-    #   @game_title = params[:subject]
-    #   @subject_image = 'rnrgntaicfrllbnrzlxg'
-    #   @science = 1
-    # end 
     if params[:subject].present?
       if params[:subject] == 'All'
       @games = Game.all
@@ -44,5 +24,15 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
     @kid = Kid.find(params[:kid_id])
+  end
+  
+  private
+ 
+  def set_kid
+    @kid = Kid.find(params[:kid_id])
+  end
+  
+  def set_game
+    @game = Game.find(params[:id])
   end
 end
