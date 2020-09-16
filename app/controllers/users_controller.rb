609c41f382364e81b6ed9587af53d6ce8a193a88
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-09-16 12:25:01 +0100

    added set kids and set user methods to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, :set_kids, only: [:profile]
+  
   def profile
     @user = current_user
+    @kids = @user.kids
+  end
+  
+  private
+ 
+  def set_user
+    @user = current_user
+  end
+  
+  def set_kids
     @kids = @user.kids
   end
 end
