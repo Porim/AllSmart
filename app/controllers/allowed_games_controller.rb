1723e4a1662623fe817100dd29a353966731a668
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-09-21 16:31:44 +0100

    now upong deletion, ajax takes user to the same row. There are however some problems with margins, might be able to fix them by using css grid

--- app/controllers/allowed_games_controller.rb
+++ app/controllers/allowed_games_controller.rb
@@ -1,8 +1,9 @@
 class AllowedGamesController < ApplicationController
+  before_action :set_kid, only: [:create, :destroy]
   #if subject is nil, after creation of allowed game user stays at the same location of the page in All Games tab - this is insured by the anchor. Else, user stays on the same location of the page, but on the corresponding tab, rather than All Games tab.
   def create
     @game = Game.find(params[:format])
-    @kid = Kid.find(params[:kid_id])
+    @delete_redirect = []
     subject = params[:subject].nil? ? 'All' : params[:subject]
     if @kid.allowed_games.find_by(game_id: @game.id).present?
       redirect_to games_path(subject: subject, anchor: "record-#{@game.id}"), notice: "Game is already added"
@@ -19,8 +20,16 @@ class AllowedGamesController < ApplicationController
 
   def destroy
     @allowed_game = AllowedGame.find(params[:id])
-    @kid = @allowed_game.kid
+    kid_games = @kid.allowed_games.where("created_at > ?", @allowed_game.created_at).empty?
+    @allowed_games = kid_games ? @kid.allowed_games.where("created_at < ?", @allowed_game.created_at).order('created_at DESC') : @kid.allowed_games.where("created_at > ?", @allowed_game.created_at).order('created_at ASC')
+    @kid = Kid.find(params[:kid_id])
     @allowed_game.destroy
-    redirect_to kid_path(@kid)
+    redirect_to kid_path(@kid, anchor: "allowed-game-#{@allowed_games.first.id}")
+  end
+  
+  private
+  
+  def set_kid
+    @kid = Kid.find(params[:kid_id])
   end
 end
