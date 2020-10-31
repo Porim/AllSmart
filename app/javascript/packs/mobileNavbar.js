8f26dc46dcb0a1bb30af1e8af9c308ecec697416
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-10-31 12:59:00 +0000

    fixed small bug application.js, prevention AOS animation working on smaller screens

--- app/javascript/packs/mobileNavbar.js
+++ app/javascript/packs/mobileNavbar.js
@@ -3,7 +3,7 @@ const fullScreenNav = () => {
   const navbarNav = document.querySelector('.navbar-nav');
   const navbarMain = document.querySelector('.navbar-main');
   const mobileRocket = document.getElementById('mobile-rocket');
-  const decolorNavbar = () => {
+  const decolorNavbarMain = () => {
     navbarMain.style.transition = '0.15s';
     navbarMain.style.background = 'none';
   }
@@ -11,13 +11,12 @@ const fullScreenNav = () => {
   navToggler.addEventListener('click', () => {
     counter += 1
     if (counter % 2 === 0 && mobileRocket) {
-      setTimeout(decolorNavbar, 300);
+      setTimeout(decolorNavbarMain, 300);
     } else {
       navbarMain.style.transition = '0.0s';
-      navbarNav.style.minHeight = '100vh';
-      navbarNav.style.minWidth = '100vw';;
-      navbarNav.style.background = '#0E91A1';
       navbarMain.style.background = '#0E91A1';
+      navbarNav.style.minHeight = '100vh';
+      navbarNav.style.background = '#0E91A1';
       document.body.style.height = '100vh';
       document.body.style.position = 'relative';
       document.body.style.overflow = 'hidden';
