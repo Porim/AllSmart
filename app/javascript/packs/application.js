e3be5d370d32f61657c8279bcf7d753888ce79dc
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-11-11 21:37:30 +0000

    sizing issues fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,7 +29,7 @@ import { ask, moveForward, displayQuestion } from './wagon_race'
 import { topFunction } from './scrollTopButtons'
 import { landingArrow } from './orangeArrow'
 import { fullScreenNav } from './mobileNavbar'
-import { mobileFormScroll } from './focusScrollAndroid'
+// import { mobileFormScroll } from './focusScrollAndroid'
 //If statements are added to prevent calling of JS functions on irrelevant pages.
 
 document.addEventListener('turbolinks:load', () => {
@@ -48,8 +48,5 @@ document.addEventListener('turbolinks:load', () => {
   }
   if (document.querySelector('.navbar-toggler')) {
     fullScreenNav();
-  }
-   if (screen.width < 428) {
-    mobileFormScroll();
   }
 });
