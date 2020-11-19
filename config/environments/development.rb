09e757bbf01a0bb71bd818eaa0e457c5b804c2fa
miroslav palka <palkamiroslav.v@gmail.com>
2020-11-19 23:12:15 +0000

    development.rb set up for email verification

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,23 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+
+  # Email verification
+  config.action_mailer.defailt_url_options = {:host => 'localhost:3000'}
+  # config.action_mailer.default_url_options = {:host => 'allsmart.uk'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.privateemail.com"
+    :port => 587,
+    :domain => "privateemail.com",
+    :authentication => :login,
+    :user_name => "support@privateemail.com",
+    :password => ENV['EMAIL_PASSWORD']
+  }
+
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
