2db30d4340dcd2db76fc3c118fc9470a6ad21632
Miroslav Palka <palkamiroslav.v@gmail.com>
2020-12-04 15:50:51 +0000

    fixing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # config.action_mailer.default_url_options = { host: "allsmart-st.herokuapp.com" }
-  config.action_mailer.default_url_options = { host: "allsmart-uk.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "www.allsmart.uk" }
   # Email verification
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
