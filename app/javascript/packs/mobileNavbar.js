00d1d1947f7836279112ed1a6d29c01f35fd22ef
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-12-08 15:23:32 +0000

    fixed javascript for kindle navbar

--- app/javascript/packs/mobileNavbar.js
+++ app/javascript/packs/mobileNavbar.js
@@ -1,15 +1,34 @@
+let clickCounter = 0;
+
+const decolorNavbarMain = () => {
+  const navbarCollapse = document.querySelector('.navbar-collapse.collapse.show');
+  const navbarNav = document.querySelectorAll('.navbar-nav');
+  const navbarMain = document.querySelector('.navbar-main');
+  navbarMain.style.background = 'url(https://res.cloudinary.com/djznzpxie/image/upload/v1598455211/mvtrrjfbxypqirgizjd5.svg)';
+  navbarNav.forEach((el) => {
+      el.style.minHeight = 'auto';
+      el.style.background = 'none';
+    })
+  navbarCollapse?.classList.remove('show');
+  clickCounter = 0;
+}
+
+
+const removeFullScreenNav = () => {
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 700 && (clickCounter % 2 >= 1)) {
+      decolorNavbarMain();
+    }
+  })
+}
+
 const fullScreenNav = () => {
   const navToggler = document.querySelector('.navbar-toggler');
   const navbarNav = document.querySelector('.navbar-nav');
   const navbarMain = document.querySelector('.navbar-main');
-  const decolorNavbarMain = () => {
-    navbarMain.style.transition = '0.15s';
-    navbarMain.style.background = 'url(https://res.cloudinary.com/djznzpxie/image/upload/v1598455211/mvtrrjfbxypqirgizjd5.svg)';
-  }
-  let counter = 0;
   navToggler.addEventListener('click', () => {
-    counter += 1
-    if (counter % 2 === 0) {
+    clickCounter += 1
+    if (clickCounter % 2 === 0) {
       setTimeout(decolorNavbarMain, 300);
     } else {
       navbarMain.style.transition = '0.0s';
@@ -22,5 +41,4 @@ const fullScreenNav = () => {
     }
   })
 }
-
-export { fullScreenNav }
+export { fullScreenNav, removeFullScreenNav, decolorNavbarMain }
