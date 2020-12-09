925b6488cc344bf55aa5d458bf6beb2f22129d50
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-12-09 11:49:44 +0000

    changed default bootstrap navbar collapsing point to 729px

--- app/javascript/packs/mobileNavbar.js
+++ app/javascript/packs/mobileNavbar.js
@@ -1,29 +1,26 @@
-const decolorNavbarMain = () => {
-    const navbarMain = document.querySelector('.navbar-main');
-    navbarMain.style.transition = '0.15s';
-    navbarMain.style.background = 'url(https://res.cloudinary.com/djznzpxie/image/upload/v1598455211/mvtrrjfbxypqirgizjd5.svg)';
-  }
-
-
 const fullScreenNav = () => {
   const navToggler = document.querySelector('.navbar-toggler');
   const navbarNav = document.querySelector('.navbar-nav');
   const navbarMain = document.querySelector('.navbar-main');
+  const decolorNavbarMain = () => {
+    navbarMain.style.transition = '0.15s';
+    navbarMain.style.background = 'url(https://res.cloudinary.com/djznzpxie/image/upload/v1598455211/mvtrrjfbxypqirgizjd5.svg)';
+  }
   let counter = 0;
   navToggler.addEventListener('click', () => {
     counter += 1
     if (counter % 2 === 0) {
-      decolorNavbarMain;
+      setTimeout(decolorNavbarMain, 300);
     } else {
-      document.body.style.height = '100vh';
-      document.body.style.position = 'relative';
-      document.body.style.overflow = 'hidden';
       navbarMain.style.transition = '0.0s';
       navbarMain.style.background = '#0E91A1';
       navbarNav.style.minHeight = '100vh';
       navbarNav.style.background = '#0E91A1';
+      document.body.style.height = '100vh';
+      document.body.style.position = 'relative';
+      document.body.style.overflow = 'hidden';
     }
   })
 }
 
-export { fullScreenNav, decolorNavbarMain }
+export { fullScreenNav }
